a3c03ac28fa65e3c42a3929d8d4e8b45e5d164c4
pepi09 <pepa.p.simeonova@abv.bg>
2014-11-04 21:47:07 +0200

    fixing tests

--- week3/graph.js
+++ week3/graph.js
@@ -4,31 +4,52 @@ var assert = require("assert");
 
 function DirectedGraph(graph){
   this.graph = graph;
+  this.path = [];
 }
 
 DirectedGraph.prototype.addEdge = function(nodeA, nodeB){
-   graph.nodeA = graph.nodeA || [];
-   graph.nodeB = graph.nodeB || [];
-   graph.nodeA.push(nodeB);
+   this.graph[nodeA] = this.graph[nodeA] || [];
+   this.graph[nodeB] = this.graph[nodeB] || [];
+   this.graph[nodeA].push(nodeB);
   }
 
 DirectedGraph.prototype.getNeighborsFor = function(node){
-    return graph.node;
+    return this.graph[node];
   }
 
 DirectedGraph.prototype.pathBetween = function(nodeA, nodeB){
-    if (graph.nodeA.contains(nodeB)){
+
+  var contains = function(arr, el){
+    return arr.some(function(elem){
+      return elem === el; });
+  }
+
+  if (contains(this.graph[nodeA],nodeB)) {
       return true;
+    }
+  //else{
+    if (this.graph[nodeA] !== []){
+        var that = this;
+        this.graph[nodeA].forEach(function(neighbor){
+
+          if (!contains(that.path,neighbor)){
+            that.path.push(neighbor);
+            if(that.pathBetween(neighbor,nodeB)){
+              return true;
+            }
+          }
+        });
     }
     else{
       return false;
     }
-
+    //}
+return false;
   }
 
 
 DirectedGraph.prototype.toString = function(){
-    return graph;
+    return JSON.stringify(this.graph);
   }
 
-module.exports = DirectedGraph
+exports.DirectedGraph = DirectedGraph
--- week3/test-graph.js
+++ week3/test-graph.js
@@ -1,22 +1,50 @@
 "use strict";
 
 var Graph = require("./graph").DirectedGraph,
-    assert = require("chai").assert;
+    assert = require("chai").assert,graph;
 
 describe('Graph', function(){
 
   beforeEach(function(){
-    var graph =  Graph({"pepa" : ["mimeto"], "mimeto" : ["pepa"]});
+    graph =  new Graph({"pepa" : ["mimi"], "mimi" : ["pepa"]});
   });
 
-
   describe("#addEdge()", function(){
-    it('should add new edge to the graph', function(){
-      var expected = {"pepa" : ["mimeto", "mimi"], "mimeto" : ["pepa"], "mimi" : []};
-      graph.addEdge("pepa", "mimi");
-      assert.deepEqual(graph,expected,"Edge added correctly!");
-      done();
+    it("should add new edge to the graph", function(){
+      var expected = {"pepa" : ["mimi", "lqlq"], "mimi" : ["pepa"], "lqlq" : []};
+
+      graph.addEdge("pepa", "lqlq");
+      assert.deepEqual(graph.graph,expected,"Edge added correctly!");
     })
 });
+
+  describe("#getNeighborsFor()", function(){
+    it("should return all neighbors of the edge", function(){
+      var expected = ["mimi", "lqlq"];
+      graph.addEdge("pepa", "lqlq");
+      assert.deepEqual(graph.getNeighborsFor("pepa"), expected, "Returning neighbors correctly!");
+    })
+  });
+
+  describe("#toString()", function(){
+    it("should print the graph",function(){
+      var expected = '{"pepa":["mimi"],"mimi":["pepa"]}';
+      assert.equal(graph.toString(),expected,"Printed correctly!");
+    })
+  });
+
+  describe("#pathBetween()",function(){
+    it("should return true if the eges are neighbors",function(){
+      assert.isTrue(graph.pathBetween("pepa","mimi"),"Path found!");
+    })
+  });
+
+  describe("#pathBetween()", function(){
+    it("should return true if there is a path between the edges", function(){
+      graph.addEdge("mimi", "lqlq");
+      console.log(graph);
+      assert.isTrue(graph.pathBetween("pepa","lqlq"),"Path found!")
+    })
+  });
 });
 
