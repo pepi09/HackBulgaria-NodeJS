cbc1a74c03be98af29c5180ecadbd97e074552ae
pepi09 <pepa.p.simeonova@abv.bg>
2015-02-11 17:42:06 +0200

    adding functions for rgb

---
+++ exam/2-Image-Filters-library/rgb.js
@@ -0,0 +1,44 @@
+"use strict"
+var Q = require("q"),
+    Monochrome = require("./monochrome").Monochrome
+
+function RGB(){
+
+}
+
+
+RGB.prototype = new Monochrome();
+
+RGB.prototype.applyKernel = function(image, kernel){
+  var deffered = Q.defer();
+  calculateRGB(this, "applyKernel", image, kernel, deffered);
+  return deffered.promise;
+}
+
+RGB.prototype.edgeDetection = function(image){
+  var deffered = Q.defer();
+  calculateRGB(this, "edgeDetection", image, deffered);
+  return deffered.promise;
+}
+
+RGB.prototype.boxBlur = function(image){
+  var deffered = Q.defer();
+  calculateRGB(this, "boxBlur", image, deffered);
+  return deffered.promise;
+}
+
+function calculateRGB(bind, method, image, kernel, deffered){
+  Monochrome.prototype[method].call(bind, image.red, kernel).done(function(red){
+    Monochrome.prototype[method].call(bind, image.green, kernel).done(function(green){
+      Monochrome.prototype[method].call(bind, image.blue, kernel).done(function(blue){
+
+        deffered.resolve({
+          red : red,
+          green : green,
+          blue : blue
+        });
+
+      });
+    });
+  });
+}
